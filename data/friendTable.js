55c073f5ed86d1454127806c1d22da127042a3ec
race-briggs <race.briggs@gmail.com>
2019-10-14 20:15:13 -0500

    changed app to use an api file instead of sql

---
+++ data/friendTable.js
@@ -0,0 +1,46 @@
+var friendTable = [
+  {
+    name: 'Race',
+    image: 'https://www.telegraph.co.uk/content/dam/news/2018/12/19/TELEMMGLPICT000183991953_trans_NvBQzQNjv4Bq8CYWv7bmnYZ51d2hDBu7lebqe2WHyoOY47YRF5kMLEI.jpeg?imwidth=450',
+    q1: 3,
+    q2: 3,
+    q3: 3,
+    q4: 3,
+    q5: 3,
+    q6: 3,
+    q7: 3,
+    q8: 3,
+    q9: 3,
+    q10: 3
+  },
+  {
+    name: 'Disagreeing Darry',
+    image: 'https://www.telegraph.co.uk/content/dam/news/2018/12/19/TELEMMGLPICT000183991953_trans_NvBQzQNjv4Bq8CYWv7bmnYZ51d2hDBu7lebqe2WHyoOY47YRF5kMLEI.jpeg?imwidth=450',
+    q1: 1,
+    q2: 1,
+    q3: 1,
+    q4: 1,
+    q5: 1,
+    q6: 1,
+    q7: 1,
+    q8: 1,
+    q9: 1,
+    q10: 1
+  },
+  {
+    name: 'Agreeing Allie',
+    image: 'https://www.telegraph.co.uk/content/dam/news/2018/12/19/TELEMMGLPICT000183991953_trans_NvBQzQNjv4Bq8CYWv7bmnYZ51d2hDBu7lebqe2WHyoOY47YRF5kMLEI.jpeg?imwidth=450',
+    q1: 5,
+    q2: 5,
+    q3: 5,
+    q4: 5,
+    q5: 5,
+    q6: 5,
+    q7: 5,
+    q8: 5,
+    q9: 5,
+    q10: 5
+  }
+]
+
+module.exports = friendTable;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,34 @@
-var connection = require('../config/connection.js');
+// var connection = require('../config/connection.js');
+
+var friendTable = require('../data/friendTable');
 
 module.exports = function(app){
 
-  app.get('/api/survey', function(req, res){
-    var query = 'SELECT * FROM friendslist';
-    connection.query(query, function(error, results){
-      res.json(results);
-    })
+  app.get('/api/friends', function(req, res){
+    res.json(friendTable);
   })
 
-  app.post('/api/survey', function (req, res) {
-    console.log(req.body);
-    var query = 'INSERT INTO friendslist (name, image, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
-    connection.query(query, [req.body.name, req.body.image, req.body.q1, req.body.q2, req.body.q3, req.body.q4, req.body.q5, req.body.q6, req.body.q7, req.body.q8, req.body.q9, req.body.q10], function (error, results) {
-      if (error) throw error;
-      res.status(201).json(results);
-    })
+  app.post('/api/friends', function(req, res){
+    console.log('Posting...')
+    friendTable.push(req.body);
+    console.log(friendTable);
+    res.json(true);
   })
+
+  // app.get('/api/survey', function(req, res){
+  //   var query = 'SELECT * FROM friendslist';
+  //   connection.query(query, function(error, results){
+  //     res.json(results);
+  //   })
+  // })
+
+  // app.post('/api/survey', function (req, res) {
+  //   console.log(req.body);
+  //   var query = 'INSERT INTO friendslist (name, image, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
+  //   connection.query(query, [req.body.name, req.body.image, req.body.q1, req.body.q2, req.body.q3, req.body.q4, req.body.q5, req.body.q6, req.body.q7, req.body.q8, req.body.q9, req.body.q10], function (error, results) {
+  //     if (error) throw error;
+  //     res.status(201).json(results);
+  //   })
+  // })
 
 }
